1402efdded396558f899763035fa13dc0862ba05
zjeaton <zjeaton@gmail.com>
2022-06-20 13:52:58 -0700

    remove gh pages deploy and deploy to cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
-const dev = process.env.NODE_ENV === 'development';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-            pages: "docs",
-            assets: "docs",
-			// trailingSlash: 'always'
-        }),
-		// prerender: {
-		// 	default: true
-		// },
-		paths: {
-			base: dev ? '' : '/secretjs-svelte',
-		}
-	}
+export default {
+  kit: {
+    adapter: adapter()
+  }
 };
-
-export default config;
