07d779267215de85d9496e20994ab7d5d2cf0223
zjeaton <zjeaton@gmail.com>
2022-06-20 13:22:47 -0700

    prepare for github pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+            pages: "docs",
+            assets: "docs",
+			// trailingSlash: 'always'
+        }),
+		// prerender: {
+		// 	default: true
+		// },
+		paths: {
+			base: dev ? '' : '/secretjs-svelte',
+		}
 	}
 };
 
